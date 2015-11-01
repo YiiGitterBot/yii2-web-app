a9e9c6848ce4320290ded4644a30bcca616addac
lavrentiev <lavrentiev.e@gmail.com>
2015-11-01 18:47:36 +0300

    Add Achievements models

---
+++ common/models/gitter/Achievements.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace common\models\gitter;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "Achievements".
+ *
+ * @property integer $id
+ * @property string $key
+ * @property string $title
+ * @property string $description
+ * @property string $chatAnnounce
+ */
+class Achievements extends ctiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'Achievements';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['key', 'title', 'chatAnnounce'], 'required'],
+            [['description', 'chatAnnounce'], 'string'],
+            [['key'], 'string', 'max' => 64],
+            [['title'], 'string', 'max' => 256]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'key' => Yii::t('app', 'Key'),
+            'title' => Yii::t('app', 'Title'),
+            'description' => Yii::t('app', 'Description'),
+            'chatAnnounce' => Yii::t('app', 'Chat Announce'),
+        ];
+    }
+}
