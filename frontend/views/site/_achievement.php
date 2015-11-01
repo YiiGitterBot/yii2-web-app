b994d21bdae294224e7a35b21781640b9e887d7d
lavrentiev <lavrentiev.e@gmail.com>
2015-11-01 21:57:05 +0300

    Fix i18n achievement

--- frontend/views/site/_achievement.php
+++ frontend/views/site/_achievement.php
@@ -8,7 +8,7 @@ use yii\bootstrap\Html;
 <div class="col-md-4">
     <div class="panel panel-default">
         <div class="panel-body">
-            <span class="label label-info pull-right"><?= Yii::t('app', '---') ?></span>
+            <span class="label label-default pull-right"><?= Yii::t('app', '{n, plural, =0{So far nobody received} =1{One person received} other{# persons received}}', ['n' => $achievement->count]) ?></span>
             <p><?= Html::encode($achievement->title) ?></p>
             <small><?= Html::encode($achievement->description) ?></small>
         </div>
--- frontend/views/site/achievements.php
+++ frontend/views/site/achievements.php
@@ -7,6 +7,7 @@ $this->title = Yii::t('app', 'Achievements');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-achievements">
+    <h1 class="text-center"><?= Yii::t('app', 'All achievements') ?></h1>
     <div class="row">
         <?php foreach ($achievements as $achievement): ?>
             <?= $this->render('_achievement', ['achievement' => $achievement]) ?>
