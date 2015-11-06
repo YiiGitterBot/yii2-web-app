8b941d425474c4aa8d6d342a7be22e27ebee6815
lavrentiev <lavrentiev.e@gmail.com>
2015-11-06 16:39:07 +0300

    Fix i18n

--- common/messages/ru-RU/app.php
+++ common/messages/ru-RU/app.php
@@ -12,7 +12,7 @@ return [
     'Enter the user name' => 'Введите имя пользователя',
     'Search by participants' => 'Поиск по участникам',
     'On your request nothing has been found' => 'По вашему запросу ничего не найдено',
-    'Profile on GitHub.Com' => 'Профиль на GitHub.Com',
+    'Profile on github.com' => 'Профиль на github.com',
     'Private message' => 'Личное сообщение',
     'Carma' => 'Карма',
     '{n, plural, =0{So far nobody received} =1{One person received} other{# persons received}}' => '{n, plural, =0{Пока никто не получил} =1{Получил 1 человек} other{Получили # человек}}'
